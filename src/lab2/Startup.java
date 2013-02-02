6892d924d0d005a1a73e5c08cf77c7d74d410f99
cgeiser@my.wctc.edu <>
2013-02-02 05:34:42 -0600

    lab2: demonstration completed, documentation completed

--- src/lab2/Startup.java
+++ src/lab2/Startup.java
@@ -1,9 +1,69 @@
 package lab2;
 
-/**
+/**Two interfaces are used in this example: ProgrammingCourse (contains
+ * requirements for the methods of 3 common variables) and
+ * Prerequisite (contains methods for the more specific prerequisite).
+ * While you must create more code, it is a very clean, easily-understood
+ * approach.
+ * 
+ * In this case (using multiple interfaces in a single class), you cannot
+ * use the Liskov Principle.  Neither of the interfaces contains information 
+ * that is common to all classes.
  *
  * @author chrisgeiser
  */
 public class Startup {
     
+    public static void main(String[] args) {
+        
+        ProgrammingCourse course1 = new IntroToProgrammingCourse(
+                "Intro to Programming", "IT-101");
+        course1.setCredits(3);
+
+        System.out.println(toString(course1));
+
+
+        IntroJavaCourse course2 = new IntroJavaCourse(
+                "Intro to Java", "IT-201");
+        course2.setCredits(4);
+        course2.setPrerequisite("Intro to Programming");
+
+        System.out.println(toString(course2, course2));
+
+
+        AdvancedJavaCourse course3 = new AdvancedJavaCourse(
+                "Advanced Java", "IT-211");
+        course3.setCredits(5);
+        course3.setPrerequisite("Intro to Java");
+    
+        System.out.println(toString(course3, course3));
+        
+    
+    }
+    
+    public static StringBuilder toString(ProgrammingCourse pc) {
+        String spacer = "    ";
+        StringBuilder info = new StringBuilder();
+        info.append(pc.getCourseName());
+        info.append(spacer);
+        info.append(pc.getCourseNumber());
+        info.append(spacer);
+        info.append(pc.getCredits());
+        return(info);
+    }
+    
+       public static StringBuilder toString(ProgrammingCourse pc,
+               Prerequisite pr) {
+        String spacer = "    ";
+        StringBuilder info = new StringBuilder();
+        info.append(pc.getCourseName());
+        info.append(spacer);
+        info.append(pc.getCourseNumber());
+        info.append(spacer);
+        info.append(pc.getCredits());
+        info.append(spacer);
+        info.append(pr.getPrerequisite());
+        return(info);
+    }
 }
+
